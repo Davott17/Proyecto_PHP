6a9832d11bc3f9927d44e85bf3b325bc36088fda
Davott17 <123947989+Davott17@users.noreply.github.com>
2023-10-30 13:30:22 +0100

    1

--- carrito.js
+++ carrito.js
@@ -61,4 +61,27 @@ for( var i = 0; i< boton.length;i++){
 });
 }
 
+
+const verCarrito = document.querySelector("#vercarrito");
+verCarrito.addEventListener("click", () => {
+    const productos = localStorage.getItem("carrito");
+    const arrayProductos = productos.split("&");
+    let html = "";
+    arrayProductos.forEach(producto => {
+        const productoJSON = JSON.parse(producto);
+        const id = productoJSON.idProducto;
+        const cantidad = productoJSON.cantidad;
+        html += `
+                <div>
+                    <h2>${id}</h2>
+                    <small>Cantidad: ${cantidad}</small>
+                </div>
+                `
+    })
+    const contenedor = document.querySelector("#producto-carrito");
+    contenedor.innerHTML = html;
+});
+
+
+
 }
